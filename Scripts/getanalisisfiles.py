00316fd55a1999b0c625015ed59982b53adcbeaf
velgueta <velgueta@uw.edu>
2024-08-06 13:52:26 -0700

    addmapand analisisfile

--- Scripts/getanalisisfiles.py
+++ Scripts/getanalisisfiles.py
@@ -268,10 +268,213 @@ def detections(cc_arr, mode='shelly'):
     det = (cc_arr - med) / mad
     return det,mad
 
+def mad_func_shelly(data):
+    median = np.median(data)
+    deviations = np.abs(data - median)
+    mad = np.median(deviations)
+    return mad
+
+'''
+process folder it is use to take all folders and the corrs and find the event that overpass a threshold.
+'''
+def process_folders(full_path, time_utc, matched_files_with_locations, template_size, output_directory):
+    try:
+        # Get a list of all folders in the base directory
+        folders = [folder for folder in os.listdir(full_path) if os.path.isdir(os.path.join(full_path, folder))]
+    except Exception as e:
+        print(f"Error listing folders in {full_path}: {e}")
+        return
+
+    # Initialize a list to store the concatenated data from each folder
+    concatenated_data_per_folder = []
+
+    # Iterate over the folders and load the .npy files
+    for folder in folders:
+        folder_path = os.path.join(full_path, folder)
+        try:
+            npy_files = [np.load(os.path.join(folder_path, file)) for file in os.listdir(folder_path) if file.endswith('.npy')]
+            concatenated_data_per_folder.append(np.concatenate(npy_files, axis=0))
+        except Exception as e:
+            print(f"Error processing folder {folder_path}: {e}")
+            continue
+
+    # Calculate MAD for each folder and define the thresholds
+    mads_per_folder = {}
+    thresholds_per_folder = {}
+    for folder, folder_data in zip(folders, concatenated_data_per_folder):
+        try:
+            mad = mad_func_shelly(folder_data)
+            threshold =  18*mad
+            mads_per_folder[folder] = np.round(mad, decimals=3)
+            thresholds_per_folder[folder] = np.round(threshold, decimals=3)
+            #print(f"MAD for folder {folder}: {mads_per_folder[folder]}")
+            print(f"Threshold for folder {folder}: {thresholds_per_folder[folder]}")
+        except Exception as e:
+            print(f"Error calculating MAD for folder {folder}: {e}")
+  
+
+    detection_times = []
+
+    for folder, folder_data in zip(folders, concatenated_data_per_folder):
+        print(f"Processing folder {folder}")
+        threshold = thresholds_per_folder[folder]
+        indices_above_threshold = np.where(np.abs(folder_data) > threshold)[0]
+        diff_indices = np.diff(indices_above_threshold)
+        group_changes = np.where(diff_indices > 20)[0]
+        detection_groups = np.split(indices_above_threshold, group_changes + 1)
+
+        detection_times_folder = []
+
+        # Match with the folder
+        matching_item = next((item for item in matched_files_with_locations if item['original_date'].replace(":", "-").replace(" ", "_") in folder), None)
+        if matching_item:
+            for group in detection_groups:
+                if len(group) > 0:
+                    first_detection_time_utc = time_utc[group[0]].strftime('%Y-%m-%d %H:%M:%S')
+                    detection_times_folder.append({
+                        'Detection Time (UTC)': first_detection_time_utc,
+                        'Longitude': matching_item['longitude'],
+                        'Latitude': matching_item['latitude'],
+                        'Folder': folder
+                    })
+            print(f"Matching item found for folder {folder}: {matching_item}")
+        else:
+            print(f"No matching item found for folder {folder}")
+
+        print(f"Detections for folder {folder}: {detection_times_folder}")
+        detection_times.extend(detection_times_folder)
+
+        # Save to CSV
+        df = pd.DataFrame(detection_times_folder)
+        df.to_csv(os.path.join(output_directory, f'detections_{folder}.csv'), index=False)
+
+    # Remove duplicates from all CSV files
+    if detection_times:  # Ensure there are detection times to process
+        all_detections = pd.DataFrame(detection_times)
+        all_detections['Detection Time (UTC)'] = pd.to_datetime(all_detections['Detection Time (UTC)'])
+        all_detections = all_detections.sort_values(by='Detection Time (UTC)')
+
+        # Remove detections that are within 10 seconds of each other
+        threshold_seconds = 8
+        unique_detections = []
+        duplicates_dict = {}
+        previous_time = None
+
+        for index, row in all_detections.iterrows():
+            detection_time = row['Detection Time (UTC)']
+            folder = row['Folder']
+            if previous_time is None or (detection_time - previous_time).total_seconds() > threshold_seconds:
+                unique_detections.append(detection_time)
+                duplicates_dict[detection_time] = [folder]
+                previous_time = detection_time
+            else:
+                duplicates_dict[previous_time].append(folder)
+
+        # Create DataFrame for unique detections and their duplicates
+        unique_detections_data = []
+        for unique_time, folders in duplicates_dict.items():
+            unique_detections_data.append({
+                'Unique Detection Time (UTC)': unique_time,
+                'Duplicate Detections (UTC)': ', '.join(folders)
+            })
+
+        unique_detections_df = pd.DataFrame(unique_detections_data)
+
+        # Print the first column of unique_detections_df
+        print("Unique Detection Times (UTC):")
+        print(unique_detections_df['Duplicate Detections (UTC)'])
+
+        unique_detections_df.to_csv(os.path.join(output_directory, 'unique_detections.csv'), index=False)
+
+        print(f"Detections saved in {output_directory}")
+    else:
+        print("No detection times to process.")
+
+'''
+This fuction is plotting the corrs values and the threshold for each folder
+'''
+
+def process_folders_and_plot(full_path, fs, time_utc, output_plot_directory, found_files):
+    try:
+        folders = [folder for folder in os.listdir(full_path) if os.path.isdir(os.path.join(full_path, folder))]
+    except Exception as e:
+        print(f"Error listing folders in {full_path}: {e}")
+        return
+
+    concatenated_data_per_folder = []
+    for folder in folders:
+        folder_path = os.path.join(full_path, folder)
+        try:
+            npy_files = [np.load(os.path.join(folder_path, file)) for file in os.listdir(folder_path) if file.endswith('.npy')]
+            concatenated_data_per_folder.append(np.concatenate(npy_files, axis=0))
+        except Exception as e:
+            print(f"Error processing folder {folder_path}: {e}")
+            continue
+
+    mads_per_folder = {}
+    thresholds_per_folder = {}
+    for folder, folder_data in zip(folders, concatenated_data_per_folder):
+        try:
+            mad = mad_func_shelly(folder_data)
+            threshold =  18*mad
+            mads_per_folder[folder] = np.round(mad, decimals=3)
+            thresholds_per_folder[folder] = np.round(threshold, decimals=3)
+            #print(f"MAD for folder {folder}: {mads_per_folder[folder]}")
+            print(f"Threshold for folder {folder}: {thresholds_per_folder[folder]}")
+        except Exception as e:
+            print(f"Error calculating MAD for folder {folder}: {e}")
 
 
+    for i, (folder, folder_data) in enumerate(zip(folders, concatenated_data_per_folder)):
+        fig, ax = plt.subplots(figsize=(15, 5))
+        ax.plot(time_utc[:len(folder_data)], folder_data, label=f'Folder {folder}', color='blue', linestyle='-')
+        ax.axhline(y=thresholds_per_folder[folder], color='red', linestyle='--', label='Threshold')
+        ax.set_title(f'Template {folder}')
+        ax.set_xlabel('Time')
+        ax.set_ylabel('Correlation Value')
+        ax.legend(loc='upper right')
+        ax.grid(True)
 
+        plot_filename = os.path.join(output_plot_directory, f'plot_{folder}.png')
+        plt.savefig(plot_filename)
+        plt.show()
+        plt.close(fig)
 
+    print(f"Plots saved in {output_plot_directory}")
+
+'''
+Fuction to know if find the same events that PNSN or not
+
+'''
+
+def compare_detection_times(unique_detections_df, matched_files_with_locations, output_directory):
+    
+
+    original_dates = [pd.to_datetime(item['original_date']) for item in matched_files_with_locations]
+
+    # Inicializar una columna para los resultados de las coincidencias
+    unique_detections_df['Match-PNSN'] = 'No'  # Todos inicializados a 'No'
+    
+    # Iterar sobre cada tiempo de detección única
+    for index, row in unique_detections_df.iterrows():
+        detection_time = pd.to_datetime(row['Unique Detection Time (UTC)'])
+        has_match = False  # Indicador para detectar si se encontró alguna coincidencia
+
+        # Comparar cada tiempo de detección con todas las fechas originales
+        for original_date in original_dates:
+            if abs((detection_time - original_date).total_seconds()) <= 10:
+                has_match = True
+                break  # Salir del bucle si se encuentra una coincidencia
+
+        # Actualizar el DataFrame con el resultado de la coincidencia
+        unique_detections_df.at[index, 'Match-PNSN'] = 'Yes' if has_match else 'No'
+
+    # Guardar el DataFrame actualizado en el directorio existente
+    output_csv_path = os.path.join(output_directory, 'resulting_detections.csv')
+    unique_detections_df.to_csv(output_csv_path, index=False)
+    
+    print(f"DataFrame saved in {output_csv_path}")
+    return unique_detections_df
 
 
 
--- Scripts/templatesmaker.py
+++ Scripts/templatesmaker.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import h5py
 import matplotlib.pyplot as plt
 from scipy.signal import butter, filtfilt, find_peaks
